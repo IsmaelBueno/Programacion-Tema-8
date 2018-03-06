e30e6f3d1916b91ac329e8d36966940388981858
Ismael Bueno <IsmaelBMolina@gmail.com>
2018-03-06 16:35:39 +0100

    Añadido el ejercicio 3

---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 3";
+
+            string ficheroorigen = "";
+            string ficherodestino = "";
+
+            Console.WriteLine("Este programa copia el contenido especificado de un fichero en otro");
+            try
+            {
+
+                Console.WriteLine("A continuación introduce la ruta de origen (absoluta o de origen), si es el directorio actual no introduzcas nada");
+                ficheroorigen = Console.ReadLine();
+                if (ficheroorigen == "")
+                {
+                    ficheroorigen = @"./";
+                }
+
+                Console.WriteLine("Ahora introduce la ruta en la que quieres copiar el archivo, si es el directorio actual no introduzcas nada");
+                ficherodestino += Console.ReadLine();
+                if (ficherodestino == "")
+                {
+                    ficherodestino = @"./";
+                }
+
+
+                //comprueba si existen los datos introducidos
+                if (!Directory.Exists(ficheroorigen))
+                {
+                    Console.WriteLine("No se pudo encontrar el directiorio de origen");
+                    Console.ReadKey();
+                    return;
+                }
+                if (!Directory.Exists(ficherodestino))
+                {
+                    Console.WriteLine("No se pudo encontrar el directorio de destino");
+                    Console.ReadKey();
+                    return;
+                }
+
+                string[] archivos = null;
+                string[] directorios = null;
+
+                archivos = Directory.GetFiles(ficheroorigen);
+                directorios = Directory.GetDirectories(ficheroorigen);
+
+                
+                //realiza la copia
+                for (int i = 0; i < archivos.Length; i++)
+                {
+                    File.Copy(archivos[i], ficherodestino + "/" + Path.GetFileName(archivos[i]));
+                }
+
+                for (int i = 0; i < directorios.Length; i++)
+			    {
+                    DirectoryInfo tmp = new DirectoryInfo(directorios[i]);
+                    Directory.CreateDirectory(ficherodestino+"/"+tmp.Name);
+                }
+
+                Console.Clear();
+                Console.WriteLine("Copia realizada con éxito");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error al realizar la copia");
+                Console.ReadKey();
+            }
+        }
+    }
+}

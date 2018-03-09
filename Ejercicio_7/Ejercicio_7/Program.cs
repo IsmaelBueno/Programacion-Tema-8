7fc8ff140824098f17b26f345a6758044ad04c0c
alumno1718 <alumno1718>
2018-03-09 10:31:31 +0100

    Añadido ejercicio 7 y 8

---
+++ Ejercicio_7/Ejercicio_7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio7";
+
+            string rutaA = @"C:/basura/salida.txt";
+            string rutaB = @"C:/basura2/salidaCopia.txt";
+
+            if(ConcatenarArchivos(rutaA, rutaB)) Console.WriteLine("Concatenación correcta");
+            else Console.WriteLine("Ha surgido un problema al intentar realizar la concatenación");
+            Console.ReadKey();
+
+        }
+
+        static bool ConcatenarArchivos(string rutaA, string rutaB)
+        {
+
+            if (!File.Exists(rutaA)) throw new Exception("Imposible encontrar la ruta de A");
+            if (!File.Exists(rutaB)) throw new Exception("Imposible encontrar la ruta de B");
+
+            try
+            {
+                string contenidoB = string.Empty;
+
+                using (StreamReader lecturaB = new StreamReader(rutaB))
+                {
+                    contenidoB = lecturaB.ReadToEnd();
+                }
+
+                using (FileStream flujoA = new FileStream(rutaA, FileMode.Append, FileAccess.Write))
+                {
+                    using (StreamWriter escrituraA = new StreamWriter(flujoA))
+                    {
+                        escrituraA.Write(contenidoB);
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ejercicio_8/Ejercicio_8/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Title = "Ejercicio 8";
+
+            string[] alumnos = new string[] {"Antonio, Pim Pam","Ismael, Bueno Molina","Adrian, Muñoz Mudarra","Tocayo, Salmeron Muñoz","Dani, Casado Molina","Jose Luis, Apellidos Imaginarios"};
+            string ruta = @"c:/basura/alumno_datos.txt";
+
+            CrearFichero(ruta, alumnos);
+            ListarContenidoFichero(ruta);
+
+            Console.ReadKey();
+        }
+
+        static bool CrearFichero(string ruta, string[] alumnos)
+        {
+            try
+            {
+                using (FileStream flujo = new FileStream(ruta, FileMode.CreateNew, FileAccess.Write))
+                {
+                    using (StreamWriter escritura = new StreamWriter(flujo))
+                    {
+                        for (int i = 0; i < alumnos.Length; i++)
+                        {
+                            escritura.WriteLine(alumnos[i] + ";");
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        static bool ListarContenidoFichero(string ruta)
+        {
+            string contenido = string.Empty;
+            try
+            {
+                //Lectura
+                using (StreamReader lectura = new StreamReader(ruta))
+                {
+                    contenido = lectura.ReadToEnd();
+                }
+
+                //Mostrar contenido
+                for (int i = 0; i < contenido.Length; i++)
+                {
+                    if (contenido[i] == ';')
+                    {
+                        Console.WriteLine();
+                    }
+                    else 
+                    {
+                        Console.Write(contenido[i]);
+                    }
+                }
+                
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
